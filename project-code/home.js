4819bdb27661ecffb5ad01e79f8c8e8382c3ac11
angelaoh01 <angelaoh2001@gmail.com>
2023-12-02 13:33:48 -0500

    fixed a lot of css

--- project-code/home.js
+++ project-code/home.js
@@ -1,5 +1,34 @@
+
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('#nav-bar');
-    var instances = M.Pushpin.init(elems, options);
+  var elems = document.querySelectorAll('.carousel');
+  let options = {
+    duration: 200,
+    dist: 0,
+    shift: 0,
+    numVisible: 1,
+    noWrap: false,
+    fullWidth: true,
+    indicators: true
+  };
+  var instances = M.Carousel.init(elems, options);
+
+  var carouselInstances = M.Carousel.getInstance(elems[0]); // Assuming only one carousel instance
+  var prevButton = document.querySelector('.left-icon');
+  var nextButton = document.querySelector('.right-icon');
+
+  prevButton.addEventListener('click', function() {
+    carouselInstances.prev();
   });
+
+  nextButton.addEventListener('click', function() {
+    carouselInstances.next();
+  });
+});
+  var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  });
+
+
+
 
