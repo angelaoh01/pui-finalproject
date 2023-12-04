a89700b8bc2560395df54ad578201323d88836c6
angelaoh01 <angelaoh2001@gmail.com>
2023-12-04 01:48:21 -0500

    little updates

--- project-code/connect.js
+++ project-code/connect.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     let options = {
       duration: 200,
-      dist: -200,
-      shift: 200,
+      dist: -100,
+      shift: 150,
       numVisible: 3,
       noWrap: false,
       fullWidth: false,
